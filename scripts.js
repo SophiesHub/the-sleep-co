a7e46d91e1c33c00cbcfd68e2a9937fa8d08463f
SophiesHub <83242104+SophiesHub@users.noreply.github.com>
2023-06-05 18:53:19 +0100

    Adding popup gallery

--- scripts.js
+++ scripts.js
@@ -13,3 +13,14 @@ $( document ).ready(function() {
         duration: 500 // values from 0 to 3000, with step 50ms
     });
 });
+
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
